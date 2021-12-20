b33e27b79b6eac831b8d10cb1d94169ebc180615
RaniHanna <79542760+RaniHanna@users.noreply.github.com>
2021-12-20 15:31:03 -0800

    First Commit
    First Commit

---
+++ inversePyramid.py
@@ -0,0 +1,11 @@
+def inversePyramid(pyramidLength = 4):
+    if(pyramidLength <= 0):
+        print('Negative')
+        return(0)
+    k = []
+    for i in range(pyramidLength):
+        k.append(1)
+    print(k)
+    inversePyramid(pyramidLength - 1)
+
+inversePyramid(13)
---
+++ probabilitySim/probabilitySim.py
@@ -0,0 +1,3 @@
+import random
+from inversePyramid.py import inversePyramid
+inversePyramid(4)
