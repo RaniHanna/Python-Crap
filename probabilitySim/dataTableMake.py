c57c3d16977caf2d146d17f277f511039a751eb1
RaniHanna <79542760+RaniHanna@users.noreply.github.com>
2021-12-21 15:03:41 -0800

    File name change
    Changed name of file & added some functionality to test pass rates

--- probabilitySim/dataTableMake.py
+++ probabilitySim/dataTableMake.py
@@ -1,16 +1,18 @@
 import random
 
+# Variable Declarations
 skill =[]
 luck = []
 aggregate = []
+tableEntry = [skill , luck , aggregate]
 
 def dataTableMake(tableSize = 10 , percentSkill = 95 , percentLuck = 5):
     if(percentSkill + percentLuck != 100):
         percentLuck += 100 - (percentSkill + percentLuck) # Will replace percentLuck with 100 - percentSkill
         # Doing this ensures our total sum will equal 100, but we send a warning message to the user 
-        # As nothing in the code breaks but the output will be off
-        print(percentLuck)
-        print(percentSkill + percentLuck)
+        # As nothing in the code breaks but the output will be off\
+        print('Your Values Will Yield An Innacurate Result, Program Still Runs')
+
     for i in range(tableSize):
         skill.append(random.random())
         skill[i] *= percentSkill
@@ -19,8 +21,16 @@ def dataTableMake(tableSize = 10 , percentSkill = 95 , percentLuck = 5):
         aggregate.append(skill[i] + luck[i])
         if(aggregate[i] < 0):
             aggregate[i] = 0
-dataTableMake(10 , 90 , 10)
-print(aggregate)
-print(luck)
-print(skill)
+           
+dataTableMake(10 , 100, 0)
+passArr = []
+numPassed = 0
+for i in range(10):
+    if(tableEntry[2][i] >= 90):
+        passArr.append(tableEntry[1][i])
+        numPassed += 1
+    else:
+        passArr.append(-1)
+print(passArr)
+print(numPassed)
 
