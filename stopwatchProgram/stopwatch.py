0ff259b8e44548c0bd81b1b6d2efd1b21154918e
Anthony Le <aznthonyle45@gmail.com>
2022-01-16 15:44:03 -0800

    Finished the backend and frontend logic. Will need to do the infinite GUI time update and CSV file saving

--- stopwatchProgram/stopwatch.py
+++ stopwatchProgram/stopwatch.py
@@ -1,37 +1,19 @@
 from datetime import datetime
+from faulthandler import disable
 import PySimpleGUI as sg
 
 
 # Starts the counter through user input of 'start'    
 def startCounter():
-    print("Please Type 'start' in the prompt below to start counting time")
-    beginCount = input()
-    
-    # If the user types 'start' then record the current time and output it
-    if(beginCount == 'start' or beginCount == 'Start'):
-        global startTime
-        startTime = datetime.now()
-        print("The start time is: " + str(startTime))
-    
-    # Else recursively call the function again 
-    else:
-        startCounter()
+    global startTime
+    startTime = datetime.now()
+    print("The start time is: " + str(startTime))
 
 # Ends the counter through user input of 'end'
 def endCounter():
-    print("When you are finished, type 'end' to finish counting time")
-    endCount = input() 
-
-    # If the user types 'start' then record the current time and output it
-    if(endCount == "end" or endCount == "End"):
         global endTime
         endTime = datetime.now()
         print("The final time is: " + str(endTime))
-
-        # Else recursively call the function again 
-    
-    else:
-        endCounter()
 
 # GUI begins :)
 
@@ -55,20 +37,58 @@ while True:
 
     if event == sg.WIN_CLOSED or event == 'Exit':
         break
+
+    # User presses start and calls the startCounter() function
+    # Record start time
+    if event == 'Start':
+        print("You have pressed the start button")
+        
+        # Change the button states
+        window['Start'].update(disabled=True)
+        window['End'].update(disabled=False)
+        
+
+        startCounter()
+
+    # Timer runs till user presses stop
+    # Stop is pressed and calls the endCounter function
+    if event == 'End':
+        print("You have pressed the end button")
+
+        # Change the button states
+        window['Start'].update(disabled=False)
+        window['End'].update(disabled=True)
+        endCounter()
+
+    # Call the datetime.now() infinitely and display it on the GUI (EQ = datime.now() - startTime)    
     print(event,values)
+
+
+# Elapsed time is calculated (end-start) and recorded to the csv file
+# timeElapsed = endTime - startTime
+
+
+# Problem with this is the CSV file saving and creation
+# After trying to save another time, the program creates another CSV file rather than appending to the one already within the directory
+"""        
+
+        try:
+            openCSV = open('openCSV.csv' , 'r')
+            openCSV.close()
+            openCSV = open('openCSV.csv' , 'a')
+            openCSV.write(str(startTime) + ", " + str(endTime) + ", " + str(timeElapsed) + "\n")
+        except:
+            openCSV = open('openCSV.csv' , 'a')
+            openCSV.write("Start Time , End Time , Time Elapsed (HH:MM:SS.ms) \n")
+            openCSV.write(str(startTime) + ", " + str(endTime) + ", " + str(timeElapsed) + "\n")
+            openCSV.close()"""
+
+
 
 # Always called like a file close (safely frees up resources)
 window.close()
 
-# User presses start and calls the startCounter() function
-# Record start time
-# Call the datetime.now() infinitely and display it on the GUI (EQ = datime.now() - startTime)
-
-# Timer runs till user presses stop
-# Stop is pressed and calls the endCounter function
 # End time is also displayed on the GUI (EQ = datime.now() - startTime)
-
-# Elapsed time is calculated (start) and recorded to the csv file
 
 #---------------------------------------------------------------------------------#
 
