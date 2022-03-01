3f464f3ab40ee8ef6018081eecd85ecf22ada06e
RaniHanna <ranihanna77@gmail.com>
2022-02-28 18:44:37 -0800

    Minor updates
    Added documentation & Semi-fixed a bug or two

--- seniorDesignGUIStuff/arduinoTest.py
+++ seniorDesignGUIStuff/arduinoTest.py
@@ -4,24 +4,45 @@ import pandas as pd
 import os
 import PySimpleGUI as sg
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import random
+import time
 
-device = serial.Serial(port='COM3', baudrate = 9600, timeout=.1)
+# Establishes serial comms w/ MCU 
+device = serial.Serial(port='COM3', baudrate = 9600, timeout=.1) 
 i = 0
 
+# Creates CSV and writes headers
 graphCSV = open('graphCSV.csv' , 'a')
 graphCSV.write('time , Voltage \n')
 graphCSV.close()
+
+# Varibles to be used in multiple functions
+runningFlag = True
 csvIndex = 0
 t = []
 V = []
 
+'''
+makeGraph() 
+    - Creates empty plot prior to GUI main loop so graph can continuously update
+    - Also sets the graph axes/limits, note that xlim corresponds to len(t), this will change to the amt allocated in the time/div
+
+generateData()
+    - Reads voltage reading from MCU's internal ADC and converts to an integer value to be stored in the CSV 
+    - CSV data is converted back to integer values and stored into the t & V lists for plotting
+
+updateGraph()
+    - Deletes the empty figure/plot made in the makeGraph portion and generates a new plot by calling the generateData() fct
+    - By calling generateData() we get the most recently updated data from the t & V lists
+    - Will be called continuously while in the GUI main loop, throws an error when not triggered via an "Update" event
+'''
 def makeGraph():
     global savePlot
+    global t
+    global V
     savePlot = plt.figure()
     plt.plot(t , V)
-    plt.ylim((0 , 1))
-    plt.xlim((0 , 3))
+    plt.ylim((0 , 5))
+    plt.xlim((0 , len(t)))
     global figManip
     figManip = draw_figure(window['Plot'].TKCanvas , savePlot)
 
@@ -37,7 +58,12 @@ def generateData():
         x *= (5 / 1023)
         print(x)
         csvIndex += 1
-
+        '''
+        # We COULD just do this instead of dealing with the CSV but I already did that work soo whatever it might come in handy
+        t.append(csvIndex)
+        V.append(x)
+        '''        
+        
         graphCSV = open('graphCSV.csv' , 'a')
         graphCSV.write(str(csvIndex) + ", " + str(x) + "\n")
         graphCSV.close()
@@ -47,7 +73,7 @@ def generateData():
         
         t.append(z[i][0])
         V.append(z[i][1])
-        print(V)
+        print(t)
         i += 1
 
 def updateGraph():
@@ -58,14 +84,13 @@ def updateGraph():
     figManip.get_tk_widget().forget()
     generateData()
     '''
+    #Old Code to generate some quick data before we ironed out the CSV parsing for data plotting
     t = [0 , 1 , 2 , 3]
     V = [random.random() , random.random() , random.random() , random.random()]
     '''
     plt.clf()
-    '''
-    plt.ylim((0 , 1))
-    plt.xlim((0 , 3))
-    '''
+    plt.ylim((0 , 5))
+    plt.xlim((0 , len(t)))
     plt.plot(t , V)
     figManip = draw_figure(window['Plot'].TKCanvas , savePlot)
 
@@ -77,22 +102,31 @@ def draw_figure(canvas, figure, loc=(0, 0)):
     return figure_canvas_agg
 # ------------------------------- Beginning of GUI CODE -------------------------------
 layout = [[sg.Canvas(key = 'Plot')],
-          [sg.Button('Update')]
+          [sg.Button('Start')],[sg.Button('Stop')],
          ]
 window = sg.Window('Virtual Oscilloscope GUI', layout, force_toplevel=True, finalize=True)
 
 makeGraph()
 
 while(True):
-    event, values = window.read(timeout = 100)
-    generateData()
-    #if event == 'Update':
-    updateGraph()
+    event, values = window.read(timeout = 50)
+
+    # NOTE: no "top level" error when the updateGraph() fct is called via 'Update' key instead of continuous calling
+
+    if event == 'Start':
+        runningFlag = True
+    if event == 'Stop':
+        runningFlag = False
+
+    if runningFlag == True:
+        generateData()
+        updateGraph()
 
     if event == sg.WIN_CLOSED:
         graphCSV.close()
+        runningFlag = False
         os.remove('graphCSV.csv')
         break
 
 window.close()
-os.remove('graphCSV.csv')
+#os.remove('graphCSV.csv')
